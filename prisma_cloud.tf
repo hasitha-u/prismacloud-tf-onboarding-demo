07b31244b09e7a3e13bf8fa7651f49a5e2a75af7
Hasitha Ubeywarna <36988425+hasitha-u@users.noreply.github.com>
2022-12-07 15:35:43 +0000

    add demo account

--- prisma_cloud.tf
+++ prisma_cloud.tf
@@ -14,3 +14,20 @@ resource "prismacloud_user_role" "test_group_role" {
   role_type         = "Account Group Admin"
   account_group_ids = [prismacloud_account_group.test_group.group_id]
 }
+
+resource "prismacloud_account_group" "demo1_group" {
+  name        = "DEMO1_GROUP"
+  description = "Made by Terraform"
+  lifecycle {
+    ignore_changes = [
+      account_ids,
+    ]
+  }
+}
+
+resource "prismacloud_user_role" "demo1_group_role" {
+  name              = "demo1"
+  description       = "Made by Terraform"
+  role_type         = "Account Group Admin"
+  account_group_ids = [prismacloud_account_group.demo1_group.group_id]
+}
