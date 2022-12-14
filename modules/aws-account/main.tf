db4e61d96b6e1c8164314f8543037c97b1dc87ac
Hasitha Ubeywarna <36988425+hasitha-u@users.noreply.github.com>
2022-12-14 01:15:00 +0000

    remove aws role

--- modules/aws-account/main.tf
+++ modules/aws-account/main.tf
@@ -11,32 +11,32 @@ resource "aws_organizations_account" "account" {
   }
 }
 
-resource "aws_iam_role" "admin" {
-  provider           = aws.sub-account
-  name               = "admin"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::${data.aws_caller_identity.root.account_id}:root"
-      },
-      "Action": "sts:AssumeRole",
-      "Condition": {}
-    }
-  ]
-}
-EOF
-  tags = {
-    "terraform-managed" : true
-  }
-}
+# resource "aws_iam_role" "admin" {
+#   provider           = aws.sub-account
+#   name               = "admin"
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "AWS": "arn:aws:iam::${data.aws_caller_identity.root.account_id}:root"
+#       },
+#       "Action": "sts:AssumeRole",
+#       "Condition": {}
+#     }
+#   ]
+# }
+# EOF
+#   tags = {
+#     "terraform-managed" : true
+#   }
+# }
 
-// Give roles the desired permissions
-resource "aws_iam_role_policy_attachment" "admin" {
-  provider   = aws.sub-account
-  role       = aws_iam_role.admin.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
+# // Give roles the desired permissions
+# resource "aws_iam_role_policy_attachment" "admin" {
+#   provider   = aws.sub-account
+#   role       = aws_iam_role.admin.name
+#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+# }
