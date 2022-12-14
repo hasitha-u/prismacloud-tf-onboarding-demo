da6a176f50ad7ee4c8b08e8b904ec8688e8fa033
Hasitha Ubeywarna <36988425+hasitha-u@users.noreply.github.com>
2022-12-14 01:10:22 +0000

    fix

--- aws_accounts.tf
+++ aws_accounts.tf
@@ -23,14 +23,14 @@ module "test_account_2" {
   ]
 }
 
-# module "demo1" {
-#   source        = "./modules/aws-account"
-#   account_name  = "demo1"
-#   email_address = "demo1@codepro.uk"
-#   admin_users = [
-#     aws-sso-scim_user.user["hasitha@codepro.uk"].id,
-#   ]
-#   prismacloud_account_groups = [
-#     #    prismacloud_account_group.test_group.group_id
-#   ]
-# }
+module "demo1" {
+  source        = "./modules/aws-account"
+  account_name  = "demo1"
+  email_address = "demo1@codepro.uk"
+  admin_users = [
+    aws-sso-scim_user.user["hasitha@codepro.uk"].id,
+  ]
+  prismacloud_account_groups = [
+    #    prismacloud_account_group.test_group.group_id
+  ]
+}
